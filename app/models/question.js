eabba7e5d3e4318eed5d9a6348c7b2fab7e372b7
Matt Rosanio <mmrosani@syr.edu>
2016-04-01 08:30:02 -0700

    Add basic user route functionality

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  content: DS.attr(),
+  author: DS.belongsTo('user', { async: true }),
+  answers: DS.hasMany('answer', { async: true }),
+  additionalNotes: DS.attr()
 });
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      user: this.store.findRecord('user', params.user_id),
+      questions: this.store.findAll('question')
+    });
+  }
 });
