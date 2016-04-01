de1456eb5422ec605049bff4e58cba1e3e868a15
Matt Rosanio <mmrosani@syr.edu>
2016-04-01 09:22:00 -0700

    Add functionality to add new answer to questions

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  answeringQuestion: false,
+  actions: {
+    showAnswerForm() {
+      this.set('answeringQuestion', true);
+    },
+    answerQuestion() {
+      var params = {
+        content: this.get('content'),
+        question: this.get('question'),
+        author: this.get('author')
+      };
+      this.set('answeringQuestion', false);
+      this.sendAction('answerQuestion', params);
+    }
+  }
 });
